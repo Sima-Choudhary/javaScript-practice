3b35519b6a493ebc90ba835975bcedaebd0dd485
Sima Choudhary <143160371+Sima-Choudhary@users.noreply.github.com>
2024-09-16 04:53:57 +0000

    variables and dataType

---
+++ 01_Basics/DataType.js
@@ -0,0 +1,15 @@
+"use strict";  // treat all js code as newer version
+
+//alert(3+3) // we are using nodejs, not browser.
+
+//number => 2 to the power 53
+//BigInt
+//String = ""
+//null 
+//undefined => standalone value
+//Sysmbol => Uniqueness
+//boolean
+
+//object
+console.log(typeof undefined)
+console.log(typeof null)
---
+++ 01_Basics/variables.js
@@ -0,0 +1,18 @@
+const accountId = 18723887
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2983947 // changes not allowed
+
+accountEmail="sima@google.com"
+accountPassword = "12kjn2332"
+accountCity = " kolkata"
+
+
+/**
+ * Preper not to use var
+ * because of the issue in block scope and functional scope
+ */
+console.table([accountId , accountEmail , accountPassword , accountCity , accountState])
